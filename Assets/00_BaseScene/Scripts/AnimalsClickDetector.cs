bb987f068bd9d7f9cf3cacd11a803b837a1acb9c
katsu-codemates <168396725+katsu-codemates@users.noreply.github.com>
2026-08-04 15:39:18 +0900

    クリックすると動物のパラメータが表示される機能を追加、CameraFocus.cs改良

--- Assets/00_BaseScene/Scripts/AnimalsClickDetector.cs
+++ Assets/00_BaseScene/Scripts/AnimalsClickDetector.cs
@@ -1,22 +1,56 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class AnimalsClickDetector : MonoBehaviour, IPointerClickHandler
 {
+    private CameraFocus cameraFocus;
+    private bool isFocusing = false;
+    [SerializeField] TextMeshProUGUI animalName;
+    private GameObject animalNameParent;
+    private void Start()
+    {
+        var mainCamera = Camera.main;
+        cameraFocus = mainCamera.GetComponent<CameraFocus>();
+        if (cameraFocus == null)
+        {
+            Debug.LogError("CameraFocusコンポーネントが見つかりません。");
+        }
+    }
+
+    void Update()
+    {
+        if (isFocusing)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                isFocusing = false;
+                animalNameParent.SetActive(false); // 動物の名前を表示するUIを非表示
+                cameraFocus.Unfocus();
+            }
+        }
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (eventData.button == PointerEventData.InputButton.Left)
+        if (eventData.button == PointerEventData.InputButton.Left && !isFocusing)
         {
             // クリックされた動物のデータを取得
             var animalData = GetComponent<AnimalData>();
-            if (animalData != null)
+            if (animalData != null && cameraFocus != null)
             {
-                GameObject mainCamera = GameObject.Find("Main Camera");
-                CameraFocus cameraFocus = mainCamera.GetComponent<CameraFocus>();
-                if (cameraFocus != null) cameraFocus.Focus(transform);
-                // クリックされた動物の名前をUIに表示する処理を追加
+                cameraFocus.StartFocus(transform); // カメラを動物にフォーカス
+                isFocusing = true;
+                SetAnimalParameter(animalData); // 動物の名前をUIに表示
+                animalNameParent = animalName.transform.parent.gameObject; // 親オブジェクトを取得
+                animalNameParent.SetActive(true); // 動物の名前を表示するUIを有効化
             }
             Debug.Log($"クリックされた動物: {animalData.animalName}");
         }
+    }
+
+    private void SetAnimalParameter(AnimalData animalData)
+    {
+        animalName.text = animalData.animalName;
     }
 }
--- Assets/00_BaseScene/Scripts/CameraFocus.cs
+++ Assets/00_BaseScene/Scripts/CameraFocus.cs
@@ -5,11 +5,46 @@ public class CameraFocus : MonoBehaviour
     public Camera mainCamera;
     public Vector3 screenPosition;   // 対象が映ってほしいスクリーン座標
 
+    private CameraMover cameraMover;
+    private Transform targetTransform;
+    private Vector3 originalPosition;
+    private bool isFocusing = false;
+
+    private void Start()
+    {
+        cameraMover = mainCamera.GetComponent<CameraMover>();
+        if (cameraMover == null)
+        {
+            Debug.LogError("CameraMoverコンポーネントが見つかりません。");
+        }
+    }
+    private void Update()
+    {
+        if (isFocusing && targetTransform != null)
+        {
+            Focus(targetTransform);
+        }
+    }
     public void Focus(Transform targetTransform)
     {
         Vector3 targetPos = targetTransform.position;
         Vector3 p = mainCamera.ScreenToWorldPoint(screenPosition);
         Vector3 offset = targetPos - p;
         this.transform.position += offset;
+    }
+
+    public void StartFocus(Transform targetTransform)
+    {
+        this.targetTransform = targetTransform;
+        originalPosition = this.transform.position;
+        cameraMover.enabled = false;
+        isFocusing = true;
+    }
+    public void Unfocus()
+    {
+        this.transform.position = originalPosition;
+        cameraMover.enabled = true;
+        isFocusing = false;
+        targetTransform = null;
     }
 }
